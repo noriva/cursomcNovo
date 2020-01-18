2950ae6e8c64076a5e2a8f49069a13d8572f48a4
noriva <jrmarson@bol.com.br>
2020-01-18 13:07:46 -0300

    O projeto foi criado

---
+++ src/main/java/com/marson/cursomcNovo/CursomcNovoApplication.java
@@ -0,0 +1,13 @@
+package com.marson.cursomcNovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcNovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcNovoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marson/cursomcNovo/CursomcNovoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marson.cursomcNovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcNovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
