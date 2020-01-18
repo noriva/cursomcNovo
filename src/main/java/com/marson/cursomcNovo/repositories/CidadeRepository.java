b837868e7a6f8e712ba1631561f529e7961cdc6d
noriva <jrmarson@bol.com.br>
2020-01-18 17:20:22 -0300

    Cidade e estado

---
+++ src/main/java/com/marson/cursomcNovo/repositories/CidadeRepository.java
@@ -0,0 +1,13 @@
+package com.marson.cursomcNovo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marson.cursomcNovo.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+	
+	
+	
+}
